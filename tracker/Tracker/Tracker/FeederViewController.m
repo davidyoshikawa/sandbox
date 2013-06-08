62ca41d20b1baca95a7c9f5f1cc5fc0e28204211
David Yoshikawa <halfyoshikawa@gmail.com>
2013-06-08 16:02:24 -0400

    basic single-page app

---
+++ tracker/Tracker/Tracker/FeederViewController.m
@@ -0,0 +1,29 @@
+//
+//  FeederViewController.m
+//  Tracker
+//
+//  Created by David Yoshikawa on 6/8/13.
+//  Copyright (c) 2013 david. All rights reserved.
+//
+
+#import "FeederViewController.h"
+
+@interface FeederViewController ()
+
+@end
+
+@implementation FeederViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ tracker/Tracker/Tracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tracker
+//
+//  Created by David Yoshikawa on 6/8/13.
+//  Copyright (c) 2013 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FeederAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FeederAppDelegate class]));
+    }
+}
