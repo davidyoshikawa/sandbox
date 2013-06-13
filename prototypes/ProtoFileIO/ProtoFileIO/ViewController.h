c1242eae7988b606cae4d293737d9b2867644edb
David Yoshikawa <halfyoshikawa@gmail.com>
2013-06-13 15:41:34 -0400

    ProtoFileIO: initial (starting from basic application - submitted text displayed in field)

---
+++ prototypes/ProtoFileIO/ProtoFileIO/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  ProtoFileIO
+//
+//  Created by David Yoshikawa on 6/13/13.
+//  Copyright (c) 2013 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+- (IBAction)updateLabel:(id)sender;
+- (IBAction)dismissKeyboard:(id)sender;
+
+@end
---
+++ prototypes/ProtoFileIO/ProtoFileIO/ViewController.m
@@ -0,0 +1,52 @@
+//
+//  ViewController.m
+//  ProtoFileIO
+//
+//  Created by David Yoshikawa on 6/13/13.
+//  Copyright (c) 2013 david. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)updateLabel:(id)sender {
+    NSString *labelText = [[NSString alloc] initWithFormat:@"Hi %@", [_textField text]];
+    [_label setText:labelText];
+    
+    // Dismiss the keyboard (on 'submit' button touch)
+    [_textField resignFirstResponder];
+}
+
+- (IBAction)dismissKeyboard:(id)sender {
+    // Dismiss the keyboard (on off-target touch via custom button in background)
+    [_textField resignFirstResponder];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    // Dismiss the keyboard (on 'return' button touch - as delegate behavior) - NOTE: electing to use method-specific parameter
+    [textField resignFirstResponder];
+    
+    // Update label (on 'return' button touch - with delegate reported parameter)
+    [self updateLabel:textField];
+    
+    return YES;
+}
+
+@end
