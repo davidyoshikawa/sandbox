a1bd0874826d9999bcc2e9a0ae92a9d6e48b9edf
David Yoshikawa <halfyoshikawa@gmail.com>
2013-06-16 13:40:39 -0400

    fileio-plist: initial (from basic)

---
+++ prototypes/fileio-plist/DYproto/DYproto/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  DYproto
+//
+//  Created by David Yoshikawa on 6/16/13.
+//  Copyright (c) 2013 medolarksoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+- (IBAction)updateLabel:(id)sender;
+- (IBAction)dismissKeyboard:(id)sender;
+
+@end
