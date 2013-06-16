5665dfa9338975a803fc1d0027ea825178f61454
David Yoshikawa <halfyoshikawa@gmail.com>
2013-06-16 14:16:14 -0400

    fileio-plist: update history view (from plist data - stored on each submission as text combined with date)

--- prototypes/fileio-plist/DYproto/DYproto/ViewController.h
+++ prototypes/fileio-plist/DYproto/DYproto/ViewController.h
@@ -12,6 +12,13 @@
 
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UITextView *historyView;
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+
+// Holds data from 'data.plist'.
+@property (strong, nonatomic) NSString *dataDictionaryPath;
+@property (strong, nonatomic) NSMutableDictionary *dataDictionary;
+@property (strong, nonatomic) NSMutableArray *favThings;
 
 - (IBAction)updateLabel:(id)sender;
 - (IBAction)dismissKeyboard:(id)sender;
--- prototypes/fileio-plist/DYproto/DYproto/ViewController.m
+++ prototypes/fileio-plist/DYproto/DYproto/ViewController.m
@@ -8,6 +8,12 @@
 
 #import "ViewController.h"
 
+// Constants used to identify plist-related data.
+NSString * const kDataPlistName           = @"data";
+NSString * const kDataPlistType           = @"plist";
+NSString * const kFavThingsDateFormat     = @"dd MMM hh:mm a";
+NSString * const kFavThingsKey            = @"favthings";
+
 @interface ViewController ()
 
 @end
@@ -17,6 +23,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    /**
+     * Assign contents of 'data.plist' to a mutable dictionary (for use in persisting entries from the UI).
+     *
+     * - Create (through Xcode) a new plist ('data.plist')
+     *   -- Create by right-clicking top level and adding new plist file to group and target of ProtoFileIO.
+     *   -- Populate with data as desired (through Xcode).
+     * - Get reference to this view controller's bundle.
+     * - Get (with the bundle) the path to our plist.
+     * - Initialize our dictionary from the plist.
+     */
+    NSBundle *vcBundle = [NSBundle bundleForClass:[self class]];
+    if ((_dataDictionaryPath = [vcBundle pathForResource:kDataPlistName ofType:kDataPlistType]))  {
+        _dataDictionary = [[NSMutableDictionary alloc] initWithContentsOfFile:_dataDictionaryPath];
+    }
+
+    // Initialize and show current history (from plist).
+    _favThings = [_dataDictionary objectForKey:kFavThingsKey];
+    NSLog(@"Initialized '%@' with %d items.", kFavThingsKey, (!_favThings ? 0 : [_favThings count]));
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,8 +51,35 @@
 }
 
 - (IBAction)updateLabel:(id)sender {
+
+    /**
+     * Updating the label(s) to demonstrate use of plist as a persistance mechanism.
+     *
+     * - Update label with submitted text.
+     * - Get date from the picker (and format).
+     *   -- Create a nice string to display.
+     * - Store submitted text and date (as message).
+     * - Update history view with plist data.
+     */
+    NSString *submittedText = [_textField text];
+
     // Update label ('update' button touched).
-    [_label setText:[_textField text]];
+    [_label setText:[[NSString alloc] initWithFormat:@"Adding '%@'", submittedText]];
+    
+    // Add date from picker to the submitted text.
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:kFavThingsDateFormat];
+    NSString *dateString = [dateFormatter stringFromDate:[_datePicker date]];
+    NSString *datedLabel = [[NSString alloc] initWithFormat:@"%@ '%@'", dateString, submittedText];
+    
+    // Persist the label text to the dictionary (newest entries at the front).
+    [_favThings insertObject:datedLabel atIndex:0];
+    [_dataDictionary setObject:_favThings forKey:kFavThingsKey];
+    [_dataDictionary writeToFile: _dataDictionaryPath atomically:YES];
+    
+    // Update history view gathered from the plist.
+    NSString *favThingsForDisplay = [_favThings componentsJoinedByString:@"\n"];
+    [_historyView setText:favThingsForDisplay];
     
     // Dismiss keyboard ('update' button touched).
     [_textField resignFirstResponder];
